4059d482d8208feba919e2cefdbfbe41b57e3be2
i2bkeit <keitaibrehima22@gmail.com>
2023-01-01 15:07:38 -0500

    ...

---
+++ vars/common.groovy
@@ -0,0 +1,16 @@
+def jenklib(string stageName) {
+  if ("${stageName}" == "Build") {
+    sh "mvn clean package"
+  }
+  else if("${stageName}" == "SonarQube Check") {
+    sh "mvn sonar:sonar"
+  }
+  else if("${stageName}" == "AprovalGate") {
+    timeout(time: 5,unit: 'DAYS'){
+      input message: 'Please revise and update me'
+    }
+    
+  }
+
+  
+}
